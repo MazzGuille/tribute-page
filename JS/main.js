2a5942293eb2fd670f58b0816b191c5e2ad6dfe2
MazzGuille <mazzcode@gmail.com>
2022-03-14 14:42:48 -0300

    animations

---
+++ JS/main.js
@@ -0,0 +1,73 @@
+window.sr = ScrollReveal();
+
+sr.reveal('body',{
+    distance: '0px',
+    opacity: 0,
+    duration: 3000
+});
+
+
+sr.reveal('.title',{
+    duration: 3000,
+    origin: 'right',
+    distance: '-100px',
+    delay: 1000,
+});
+
+sr.reveal('.subtitle',{
+    duration: 3000,
+    origin: 'left',
+    distance: '-100px',
+    delay: 1000,
+});
+
+
+sr.reveal('.boda',{
+    distance: '0px',
+    opacity: 0,
+    duration: 3000,
+    delay: 1500
+});
+
+sr.reveal('.embarazo',{
+    distance: '0px',
+    opacity: 0,
+    duration: 3000,
+    delay: 2000
+});
+
+sr.reveal('.playa',{
+    distance: '0px',
+    opacity: 0,
+    duration: 3000,
+    delay: 2500
+});
+
+sr.reveal('.caracolis',{
+    distance: '0px',
+    opacity: 0,
+    duration: 3000,
+    delay: 3000
+});
+
+sr.reveal('.amigas',{
+    distance: '0px',
+    opacity: 0,
+    duration: 3000,
+    delay: 3500
+});
+
+sr.reveal('.grad',{
+    distance: '0px',
+    opacity: 0,
+    duration: 3000,
+    delay: 3700
+});
+
+
+sr.reveal('.cronologia',{
+    duration: 2500,
+    origin: 'top',
+    distance: '-100px',
+    reset: true
+});
